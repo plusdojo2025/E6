ef79aa8a028cac5c139c6fdaa43434a840d117ba
plusdojo2025-yamamoto <yamamoto-takashi-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-11 13:49:46 +0900

    Update Send.java

--- source/src/main/java/dto/Send.java
+++ source/src/main/java/dto/Send.java
@@ -5,12 +5,55 @@ import java.io.Serializable;
 public class Send implements Serializable {
 	private int regist_number; // 登録番号
 	private int hold_coin; //所持コイン
-	private int recieve_coin; //
+	private int receive_coin; //
 	private int ranking_coin; //氏名
 
-	public  {
-		
-		
+	public Send(int regist_number,int hold_coin,int receive_coin,int ranking_coin){
+		this.regist_number= regist_number;
+		this.hold_coin=hold_coin;
+		this.receive_coin=receive_coin;
+		this.ranking_coin=ranking_coin;
 	}
+	
+	public Send() {
+		this.regist_number = 0;
+		this.hold_coin=0;
+		this.receive_coin=0;
+		this.ranking_coin=0;
+	}
+
+	public int getRegist_number() {
+		return regist_number;
+	}
+
+	public void setRegist_number(int regist_number) {
+		this.regist_number = regist_number;
+	}
+
+	public int getHold_coin() {
+		return hold_coin;
+	}
+
+	public void setHold_coin(int hold_coin) {
+		this.hold_coin = hold_coin;
+	}
+
+	public int getReceive_coin() {
+		return receive_coin;
+	}
+
+	public void setReceive_coin(int receive_coin) {
+		this.receive_coin = receive_coin;
+	}
+
+	public int getRanking_coin() {
+		return ranking_coin;
+	}
+
+	public void setRanking_coin(int ranking_coin) {
+		this.ranking_coin = ranking_coin;
+	}
+	
+	
 
 }
