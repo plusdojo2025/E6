8764415c6953ebbcb9a73e322a16080617b16cf8
plusdojo2025-hanyu <hanyu-chika-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-11 13:20:34 +0900

    a

--- source/src/main/java/servlet/MenuServlet.java
+++ source/src/main/java/servlet/MenuServlet.java
@@ -1,10 +1,32 @@
 package servlet;
 
-public class MenuServlet {
+import java.io.IOException;
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+
+@WebServlet("/MenuServlet")
+public class MenuServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/E6/LoginServlet");
+			return;
+		}
+
+		// メニューページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/webapp/jsp/menu.jsp");
+		dispatcher.forward(request, response);
 	}
-
 }
--- source/src/main/java/servlet/UpdateDeleteServlet.java
+++ source/src/main/java/servlet/UpdateDeleteServlet.java
@@ -1,10 +1,29 @@
 package servlet;
 
-public class UpdateDeleteServlet {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+
+@WebServlet("/UpdateDeleteServlet")
+public class UpdateDeleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/webappAns/LoginServlet");
+			return;
+		}
 	}
-
 }
