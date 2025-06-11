50e105dd19366b2225d0bf74769b023956aa11fe
plusdojo2025-yuasa <yuasa-yuto-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-11 10:57:30 +0900

    test

--- source/src/main/java/dto/Coin.java
+++ source/src/main/java/dto/Coin.java
@@ -3,7 +3,7 @@ package dto;
 public class Coin {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		// TODO 自動生成されたメソッド・
 
 	}
 
--- source/src/main/java/dto/User.java
+++ source/src/main/java/dto/User.java
@@ -3,7 +3,7 @@ package dto;
 public class User {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		// TODO 自動生成されたメ
 
 	}
 
