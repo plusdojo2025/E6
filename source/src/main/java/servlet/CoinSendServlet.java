b7c0f68af3fdccfca5df9708460fafc780af415d
plusdojo2025-hanyu <hanyu-chika-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-11 15:58:43 +0900

    puru

--- source/src/main/java/servlet/CoinSendServlet.java
+++ source/src/main/java/servlet/CoinSendServlet.java
@@ -1,10 +1,49 @@
 package servlet;
+ 
+import java.io.IOException;
 
-public class CoinSendServlet {
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+@WebServlet("/CoinSendServlet")
+public class CoinSendServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// 登録ページにフォワードする
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/Webapp/jsp/send.jsp");
+				dispatcher.forward(request, response);
+			}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/E6/LoginServlet");
+			return;
+		}
+		// リクエストパラメータを取得する
+				request.setCharacterEncoding("UTF-8");
+				String company = request.getParameter("name");
+				String department = request.getParameter("");
+				String position = request.getParameter("");
+			
 
+				// 登録処理を行う
+				BcDAO bDao = new BcDAO();
+				if (bDao.insert(new Bc(0, company,  department, position, name, zipcode, address, 
+						phone, fax, email, remarks))) { // 登録成功
+				
+				} else { // 登録失敗
+					
+				}
 	}
-
+	
+	
 }
