6cf2e6a490f7073a2d501842f11b8901b55241cf
plusdojo2025-yuasa <yuasa-yuto-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-12 13:05:40 +0900

    あ

--- source/src/main/java/servlet/UpdateDeleteServlet.java
+++ source/src/main/java/servlet/UpdateDeleteServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import dao.UsersDao;
+import dto.User;
 
 
 
@@ -31,7 +32,7 @@ public class UpdateDeleteServlet extends HttpServlet {
 
 		//リクエストパラメータを取得する
 		request.setCharacterEncoding("UTF-8");
-		int number = Integer.parseInt(request.getParameter("number"));
+		int regist_number = Integer.parseInt(request.getParameter("regist_number"));
 		String mail = request.getParameter("mail");
 		String password = request.getParameter("password");
 		String name = request.getParameter("name");
@@ -41,7 +42,7 @@ public class UpdateDeleteServlet extends HttpServlet {
 		// 更新または削除を行う
 		UsersDao bDao = new UsersDao();
 		if (request.getParameter("submit").equals("更新")) {
-			if (bDao.update(new Users(regist_number, mail,  password, name, company))) { // 更新成功
+			if (bDao.user_data_up(new User(regist_number, mail,  password, name, company))) { // 更新成功
 				
 			} else { // 更新失敗
 				
