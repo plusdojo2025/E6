e4ee4e2421d3b32abd30a53aadf50aa366e24690
plusdojo2025-yuasa <yuasa-yuto-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-13 16:15:12 +0900

    あ

--- source/src/main/webapp/js/regist_data.js
+++ source/src/main/webapp/js/regist_data.js
@@ -1,3 +1,11 @@
-if (result){
-alert('登録に成功しました！ログインページに戻ります。');
-}
+document.getElementById("regist_form").addEventListener("submit", function(event) {
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const company = document.getElementById("company").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    if (!name || !email || !company || !password) {
+      alert("すべての必須項目を入力してください。");
+      event.preventDefault(); // フォーム送信を停止
+    }
+  });
